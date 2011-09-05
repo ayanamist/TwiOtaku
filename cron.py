e9bef786d4f12a7b40948970388a93a626f06179
ayanamist <ayanamist@gmail.com>
2011-09-05 20:45:29 +0800

    cron job use thread pool

--- cron.py
+++ cron.py
@@ -2,6 +2,7 @@ import traceback
 import logging
 from StringIO import StringIO
 from threading import Thread
+from Queue import Queue, Empty
 
 import db
 import twitter
@@ -9,90 +10,94 @@ from worker import Job
 from config import OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET
 
 def cron_start(queues):
-  threads = list()
+  cron_queue = Queue()
+  for _ in range(5):
+    t = Thread(target=cron_job, args=(cron_queue, ))
+    t.setDaemon(True)
+    t.start()
   for user in db.get_all_users():
     if user['access_key'] and user['access_secret']:
-      t = Thread(target=cron_job, args=(queues[user['jid']], user))
-      t.setDaemon(True)
-      t.start()
-      threads.append(t)
-  for t in threads:
-    t.join()
+      cron_queue.put((queues[user['jid']], user))
+  cron_queue.join()
 
 
-def cron_job(queue, user):
-  jid = user['jid']
-  if not user:
-    return
+def cron_job(cron_queue):
+  while True:
+    try:
+      queue, user = cron_queue.get(True, 3)
+    except Empty:
+      return
+    jid = user['jid']
 
-  api = twitter.Api(consumer_key=OAUTH_CONSUMER_KEY,
-                    consumer_secret=OAUTH_CONSUMER_SECRET,
-                    access_token_key=user['access_key'],
-                    access_token_secret=user['access_secret'])
-  try:
-    screen_name = api.verify_credentials()['screen_name']
-  except twitter.TwitterAuthenticationError:
-    db.update_user(jid=jid, access_key=None, access_secret=None)
-    return
-  if screen_name != user['screen_name']:
-    user['screen_name'] = screen_name
-    db.update_user(jid=jid, screen_name=screen_name)
+    api = twitter.Api(consumer_key=OAUTH_CONSUMER_KEY,
+                      consumer_secret=OAUTH_CONSUMER_SECRET,
+                      access_token_key=user['access_key'],
+                      access_token_secret=user['access_secret'])
+    try:
+      screen_name = api.verify_credentials()['screen_name']
+    except twitter.TwitterAuthenticationError:
+      db.update_user(jid=jid, access_key=None, access_secret=None)
+      return
+    if screen_name != user['screen_name']:
+      user['screen_name'] = screen_name
+      db.update_user(jid=jid, screen_name=screen_name)
 
-  logger = logging.getLogger('cron')
+    logger = logging.getLogger('cron')
 
-  try:
-    if user['timeline'] & db.MODE_DM:
-      data = api.get_direct_messages(since_id=user['last_dm_id'])
-      if data and isinstance(data, list) and isinstance(data[0], twitter.DirectMessage):
-        user['last_dm_id'] = data[0]['id_str']
-        db.update_user(jid=jid, last_dm_id=user['last_dm_id'])
-        queue.put(Job(jid, data=data, allow_duplicate=False, always=False))
-  except BaseException:
-    err = StringIO()
-    traceback.print_exc(file=err)
-    logger.error(err.getvalue())
+    try:
+      if user['timeline'] & db.MODE_DM:
+        data = api.get_direct_messages(since_id=user['last_dm_id'])
+        if data and isinstance(data, list) and isinstance(data[0], twitter.DirectMessage):
+          user['last_dm_id'] = data[0]['id_str']
+          db.update_user(jid=jid, last_dm_id=user['last_dm_id'])
+          queue.put(Job(jid, data=data, allow_duplicate=False, always=False))
+    except BaseException:
+      err = StringIO()
+      traceback.print_exc(file=err)
+      logger.error(err.getvalue())
 
-  try:
-    if user['timeline'] & db.MODE_MENTION:
-      data = api.get_mentions(since_id=user['last_mention_id'])
-      if data and isinstance(data, list) and isinstance(data[0], twitter.Status):
-        user['last_mention_id'] = data[0]['id_str']
-        db.update_user(jid=jid, last_mention_id=user['last_mention_id'])
-        queue.put(Job(jid, data=data, allow_duplicate=False, always=False))
-  except BaseException:
-    err = StringIO()
-    traceback.print_exc(file=err)
-    logger.error(err.getvalue())
+    try:
+      if user['timeline'] & db.MODE_MENTION:
+        data = api.get_mentions(since_id=user['last_mention_id'])
+        if data and isinstance(data, list) and isinstance(data[0], twitter.Status):
+          user['last_mention_id'] = data[0]['id_str']
+          db.update_user(jid=jid, last_mention_id=user['last_mention_id'])
+          queue.put(Job(jid, data=data, allow_duplicate=False, always=False))
+    except BaseException:
+      err = StringIO()
+      traceback.print_exc(file=err)
+      logger.error(err.getvalue())
 
-  try:
-    if user['timeline'] & db.MODE_LIST:
-      if user['list_user'] and user['list_id']:
-        try:
-          data = api.get_list_statuses(user=user['list_user'], id=user['list_id'], since_id=user['last_list_id'])
-        except twitter.TwitterNotFoundError:
-          user['timeline'] &= ~db.MODE_LIST
-          db.update_user(id=user['id'], timeline=user['timeline'])
-          queue.put(Job(user['jid'], title='List %s/%s not exists, disable List update.' % (user['list_user'], user['list_name'])))
-        else:
-          if data and isinstance(data, list) and isinstance(data[0], twitter.Status):
-            user['last_list_id'] = data[0]['id_str']
-            db.update_user(jid=jid, last_list_id=user['last_list_id'])
-            queue.put(Job(jid, data=data, allow_duplicate=False, always=False))
-  except BaseException:
-    err = StringIO()
-    traceback.print_exc(file=err)
-    logger.error(err.getvalue())
+    try:
+      if user['timeline'] & db.MODE_LIST:
+        if user['list_user'] and user['list_id']:
+          try:
+            data = api.get_list_statuses(user=user['list_user'], id=user['list_id'], since_id=user['last_list_id'])
+          except twitter.TwitterNotFoundError:
+            user['timeline'] &= ~db.MODE_LIST
+            db.update_user(id=user['id'], timeline=user['timeline'])
+            queue.put(Job(user['jid'], title='List %s/%s not exists, disable List update.' % (user['list_user'], user['list_name'])))
+          else:
+            if data and isinstance(data, list) and isinstance(data[0], twitter.Status):
+              user['last_list_id'] = data[0]['id_str']
+              db.update_user(jid=jid, last_list_id=user['last_list_id'])
+              queue.put(Job(jid, data=data, allow_duplicate=False, always=False))
+    except BaseException:
+      err = StringIO()
+      traceback.print_exc(file=err)
+      logger.error(err.getvalue())
 
-  try:
-    if user['timeline'] & db.MODE_HOME:
-      data = api.get_home_timeline(since_id=user['last_home_id'])
-      if data and isinstance(data, list) and isinstance(data[0], twitter.Status):
-        user['last_home_id'] = data[0]['id_str']
-        db.update_user(jid=jid, last_home_id=user['last_home_id'])
-        queue.put(Job(jid, data=data, allow_duplicate=False, always=False))
-  except BaseException:
-    err = StringIO()
-    traceback.print_exc(file=err)
-    logger.error(err.getvalue())
+    try:
+      if user['timeline'] & db.MODE_HOME:
+        data = api.get_home_timeline(since_id=user['last_home_id'])
+        if data and isinstance(data, list) and isinstance(data[0], twitter.Status):
+          user['last_home_id'] = data[0]['id_str']
+          db.update_user(jid=jid, last_home_id=user['last_home_id'])
+          queue.put(Job(jid, data=data, allow_duplicate=False, always=False))
+    except BaseException:
+      err = StringIO()
+      traceback.print_exc(file=err)
+      logger.error(err.getvalue())
 
+    cron_queue.task_done()
 
