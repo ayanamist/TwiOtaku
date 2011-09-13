10359c0eac5dc3e802bb31575cd8d107ca3eeb58
ayanamist <ayanamist@gmail.com>
2011-09-13 15:36:32 +0800

    use httplib2 instead of urllib3 because twitter has banned http 1.1 keep-alive access change supervisord.conf

--- lib/urlfetch.py
+++ lib/urlfetch.py
@@ -1,22 +1,13 @@
-from urllib3 import HTTPConnectionPool, HTTPSConnectionPool, get_host
+import httplib2
 
-_pool = dict()
+_h = httplib2.Http()
 
-def fetch(url, method='GET', body=None, headers=None, retries=3, redirect=True, assert_same_host=True):
-  scheme, host, port = get_host(url)
-  scheme = scheme.lower()
-  host = host.lower()
-  if scheme == 'https' and port == 443:
-    port = None
-  elif scheme == 'http' and port == 80:
-    port = None
-  hash_result = hash((scheme, host, port))
-  pool = _pool.get(hash_result)
-  if not pool:
-    if scheme == 'https':
-      pool = HTTPSConnectionPool(host, port=port)
-    else:
-      pool = HTTPConnectionPool(host, port=port)
-    _pool[hash_result] = pool
-  return pool.urlopen(url=url, method=method, body=body, headers=headers, retries=retries, redirect=redirect,
-    assert_same_host=assert_same_host)
+class Response(httplib2.Response):
+  def __init__(self, resp, content):
+    super(Response, self).__init__(resp)
+    self.data = content
+
+
+def fetch(url, method='GET', body=None, headers=None):
+  resp, content = _h.request(uri=url, method=method, body=body, headers=headers)
+  return Response(resp, content)
