816f50a375b92772d10b2d234ecf0e974b1e14ee
ayanamist <ayanamist@gmail.com>
2011-09-14 15:00:06 +0800

    fix bug that short_id won't be recognized correctly if starts with sharp

--- lib/util.py
+++ lib/util.py
@@ -195,7 +195,7 @@ class Util(object):
     try:
       short_id = int(g)
     except ValueError:
-      short_id = alpha_to_digit(short_id)
+      short_id = alpha_to_digit(g)
     if short_id < MAX_ID_LIST_NUM:
       return db.get_long_id_from_short_id(self._user['id'], short_id)
     else:
