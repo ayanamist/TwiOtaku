5cdf532f7d9cdffc6bb451070fedc3629682dc63
ayanamist <ayanamist@gmail.com>
2011-10-25 13:50:39 +0800

    refactor

--- db/__init__.py
+++ db/__init__.py
@@ -11,39 +11,33 @@ MODE_TRACK = 32
 TYPE_STATUS = 0
 TYPE_DM = 1
 
-global add_invite_code
-global add_status
-global add_user
-global delete_invite_code
-global get_all_users
-global get_invite_code
-global get_long_id_from_short_id
-global get_short_id_from_long_id
-global get_status
-global get_user_from_jid
-global get_users_count
-global purge_old_statuses
-global update_long_id_from_short_id
-global update_user
+dbapi = __import__(DATABASE_TYPE.lower(), globals=globals(), locals=locals())
 
-_db_type = DATABASE_TYPE.lower()
-_vars = globals()
-if _db_type == 'sqlite':
-  import sqlite
+add_invite_code = dbapi.add_invite_code
 
-  _vars['add_invite_code'] = sqlite.add_invite_code
-  _vars['add_status'] = sqlite.add_status
-  _vars['add_user'] = sqlite.add_user
-  _vars['delete_invite_code'] = sqlite.delete_invite_code
-  _vars['get_all_users'] = sqlite.get_all_users
-  _vars['get_invite_code'] = sqlite.get_invite_code
-  _vars['get_long_id_from_short_id'] = sqlite.get_long_id_from_short_id
-  _vars['get_short_id_from_long_id'] = sqlite.get_short_id_from_long_id
-  _vars['get_status'] = sqlite.get_status
-  _vars['get_user_from_jid'] = sqlite.get_user_from_jid
-  _vars['get_users_count'] = sqlite.get_users_count
-  _vars['purge_old_statuses'] = sqlite.purge_old_statuses
-  _vars['update_long_id_from_short_id'] = sqlite.update_long_id_from_short_id
-  _vars['update_user'] = sqlite.update_user
-else:
-  raise NotImplementedError
+add_status = dbapi.add_status
+
+add_user = dbapi.add_user
+
+delete_invite_code = dbapi.delete_invite_code
+
+get_all_users = dbapi.get_all_users
+
+get_invite_code = dbapi.get_invite_code
+
+get_long_id_from_short_id = dbapi.get_long_id_from_short_id
+
+get_short_id_from_long_id = dbapi.get_short_id_from_long_id
+
+get_status = dbapi.get_status
+
+get_user_from_jid = dbapi.get_user_from_jid
+
+get_users_count = dbapi.get_users_count
+
+purge_old_statuses = dbapi.purge_old_statuses
+
+update_long_id_from_short_id = dbapi.update_long_id_from_short_id
+
+update_user = dbapi.update_user
+
