91525cc25c4495ee81f42b6e25502340e627b027
ayanamist <ayanamist@gmail.com>
2011-11-25 12:58:07 +0800

    fix bug: messy status when has link

--- lib/urlwrapper.py
+++ lib/urlwrapper.py
@@ -18,27 +18,19 @@
 from bisect import bisect
 
 class URLUnwrapper(object):
-  _cache_str = ''
-  _cached = False
-  _str_list = list()
-  _str_indices = [0]
-
   def __init__(self, s):
     self.original_s = s
-    self._cache_str = s
+    self._str_list = list()
+    self._str_indices = [0]
 
   def __unicode__(self):
-    if self._cached:
-      return self._cache_str
     if self._str_list:
       result = list()
       for i, s in enumerate(self._str_list):
         result.append(self.original_s[self._str_indices[i * 2]:self._str_indices[i * 2 + 1]])
         result.append(s)
       result.append(self.original_s[self._str_indices[-1]:])
-      self._cache_str = u''.join(result)
-      self._cached = True
-      return self._cache_str
+      return u''.join(result)
     else:
       return unicode(self.original_s)
 
@@ -46,7 +38,6 @@ class URLUnwrapper(object):
     return unicode(self).encode('UTF8')
 
   def replace_indices(self, start, stop, replace_text):
-    self._cached = False
     i = bisect(self._str_indices, start)
     self._str_indices.insert(i, start)
     self._str_indices.insert(i + 1, stop)
